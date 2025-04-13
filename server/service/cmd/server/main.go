4f3609a9bbcbfcedeb9ed9634d6ca66d534eb54f
jsndz <jaisondz9360@gmail.com>
2025-04-13 16:44:51 +0530

    env and db

--- server/service/cmd/server/main.go
+++ server/service/cmd/server/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/readit/pkg/db"
+)
+
+func getEnv(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	return val
+}
+
+
+func main() {
+	app := fiber.New()
+	dbConn,err := db.InitDB()
+
+
+	if err!=nil {
+		fmt.Println(err)
+	}
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	port := getEnv("PORT","3000")
+	fmt.Println("Connected:", dbConn)
+	fmt.Println("Server running on port:", port)
+	if err := app.Listen(":" + port); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
--- server/service/pkg/db/client.go
+++ server/service/pkg/db/client.go
@@ -1,3 +1,22 @@
 package db
 
-// Add valid import paths here
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+func InitDB() (*gorm.DB,error){
+	dsn := "host=localhost user=postgres password=readit dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+
+
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Coudn't run postgres")
+	}
+	return db,nil
+
+}
