556eed9e6371e1c1e395179c48cc3bb8780503e6
jsndz <jaisondz9360@gmail.com>
2025-04-14 18:18:03 +0530

    post repository

--- server/service/internal/app/model/category.go
+++ server/service/internal/app/model/category.go
@@ -3,6 +3,6 @@ package model
 type Category struct {
 	ID    uint   `gorm:"primaryKey"`
 	Name  string `gorm:"unique;not null"`
-
+	
 	Posts []Post `gorm:"foreignKey:CategoryID"`
 }
--- server/service/internal/app/repository/post_repository.go
+++ server/service/internal/app/repository/post_repository.go
@@ -1,3 +1,45 @@
-package db
+package repository
 
-// Add valid import paths here
+import (
+	"github.com/readit/internal/app/model"
+	"gorm.io/gorm"
+)
+
+type PostRepository struct{
+	db *gorm.DB
+}
+
+
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{db: db}
+}
+
+func (r *PostRepository) Create(post *model.Post) error {
+	return r.db.Create(post).Error
+}
+
+func (r *PostRepository) Read(ID string) (*model.Post, error) {
+    var post model.Post
+
+	err := r.db.Preload("Comments").First(&post, "ID = ?", ID).Error
+    if err != nil {
+        return nil, err 
+    }
+    return &post, nil
+}
+
+
+func (r *PostRepository) Update(ID string,data map[string]any) (*model.Post,error){
+	var post model.Post
+	if err:= r.db.Model(&post).Where("ID = ?", ID).Updates(data).Error; err!=nil{
+		return nil, err
+	}
+	r.db.First(&post, ID)
+	return &post,nil
+}
+
+func (r *PostRepository) Delete(ID string) (error){
+	var post model.Post
+	return  r.db.Delete(&post,ID).Error
+	
+}
