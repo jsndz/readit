efb99f05839babc24bf100ce6c04c594f1af7906
jsndz <jaisondz9360@gmail.com>
2025-04-16 11:18:08 +0530

    auth handler

---
+++ server/auth/internal/app/handler/user_handler.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jsndz/readit/auth/internal/app/model"
+	"github.com/jsndz/readit/auth/internal/app/service"
+	"gorm.io/gorm"
+)
+
+type UserHandler struct{
+	userService *service.UserService
+}
+
+func NewUserHandler(db *gorm.DB) * UserHandler {
+	return &UserHandler{
+		userService: service.NewUserService(db),
+	}
+}
+
+func (h *UserHandler) Signup (c *fiber.Ctx) error{
+	var req model.User
+
+	if err:= c.BodyParser(&req);err!=nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"data":    nil,
+			"message": "Invalid request body",
+			"success": false,
+			"err":     err.Error(),
+		})
+	}
+	token, err := h.userService.Signup(req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"data":    nil,
+			"message": "Couldn't create new user",
+			"success": false,
+			"err":     err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+		"data":    token,
+		"message": "Successfully created a new user",
+		"success": true,
+		"err":     nil,
+	}) 
+}
+
+
+func (h *UserHandler) Signin (c *fiber.Ctx) error{
+	var req model.User
+
+	if err:= c.BodyParser(&req);err!=nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"data":    nil,
+			"message": "Invalid request body",
+			"success": false,
+			"err":     err.Error(),
+		})
+	}
+	token, err := h.userService.Signin(req.Email,req.Password)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"data":    nil,
+			"message": "Couldn't signin user.",
+			"success": false,
+			"err":     err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+		"data":    token,
+		"message": "User Successfully signed in.",
+		"success": true,
+		"err":     nil,
+	}) 
+}
