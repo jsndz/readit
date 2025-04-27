9ac3a0e2e676d834279349e69b798eeac3e278a0
jsndz <jaisondz9360@gmail.com>
2025-04-27 15:09:08 +0530

    get username with post

--- server/gateway/cmd/server/main.go
+++ server/gateway/cmd/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/jsndz/readit/api-gateway/middleware"
@@ -31,12 +34,48 @@ func main() {
 	app.All("/api/auth/*", func(c *fiber.Ctx) error {
 		return proxy.ForwardOnly(c, "http://localhost:3001")
 	})
+	app.Get("/api/post/get/:id",middleware.Authenticate,func( c *fiber.Ctx)error{
+		response, err := proxy.ForwardReturn(c, "http://localhost:3000")
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "failed to fetch post data",
+			})
+		}
+		dataMap, ok := response["data"].(map[string]interface{})
+		log.Infof("map %v",dataMap)
+		if !ok {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "invalid data format",
+			})
+		}
+
+		userId, ok := dataMap["UserID"].(float64)
+    if !ok {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error": "invalid UserID format",
+        })
+    }
+		username ,err:= proxy.ForwardSpecific(c, `http://localhost:3001/api/auth/username/`+strconv.FormatFloat(userId, 'f', -1, 64))
+		 if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "failed to fetch username",
+			})
+		}
+		 return  c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+			"data":    dataMap,
+			"username":username,
+			"message": "User Successfully signed in.",
+			"success": true,
+			"err":     nil,
+		}) 
+	})
 	app.All("/api/post/*", middleware.Authenticate, func(c *fiber.Ctx) error {
 		return proxy.Forward(c, "http://localhost:3000")
 	})
 	app.All("/api/comment/*", middleware.Authenticate, func(c *fiber.Ctx) error {
 		return proxy.Forward(c, "http://localhost:3000")
 	})
+	
 	app.Listen(":8080")
 }
 
--- server/gateway/proxy/gateway.go
+++ server/gateway/proxy/gateway.go
@@ -89,3 +89,68 @@ func ForwardOnly(c *fiber.Ctx, target string) error {
 
 	return c.Status(resp.StatusCode).JSON(responseBody)
 }
+
+func ForwardSpecific(c *fiber.Ctx, target string) (map[string]interface{},error) {
+	req, err := http.NewRequest(http.MethodGet, target, nil)
+	if err != nil {
+		return nil,err
+	}
+
+	
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil,err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil,err
+	}
+
+	var responseBody map[string]interface{}
+	if err := json.Unmarshal(bodyBytes, &responseBody); err != nil {
+		return nil,err
+	}
+
+	return responseBody,nil
+}
+
+
+
+
+func ForwardReturn(c *fiber.Ctx, target string) (map[string]interface{},error){
+	req,err:= http.NewRequest(c.Method(),target+c.OriginalURL(),bytes.NewReader(c.Body()))
+	if(err!=nil){
+		return nil,err
+	}
+
+	c.Request().Header.VisitAll(func (key,value[]byte){
+		req.Header.Set(string(key),string(value))
+	})
+
+	userID := c.Locals("id")
+	if userID != nil {
+		req.Header.Set("X-User-ID", userID.(string))
+	}
+
+	client := http.Client{}
+	resp,err := client.Do(req)
+	if err!= nil{
+		return nil,err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err!= nil{
+		return nil,err
+	}
+	var responseBody map[string]interface{}
+	if err := json.Unmarshal(body, &responseBody); err != nil {
+		return nil, err
+	}
+	return responseBody,nil
+
+}
